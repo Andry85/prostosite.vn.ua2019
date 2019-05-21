82415a3a367f68355d26b9b8ea3edbc452cb6fe4
Andrii Shapoval <shapovala@ukr.net>
2019-05-21 05:08:32 +0300

    Updates

--- precache-manifest.18d55c17aced82bd1de70f080711e4f5.js
+++ precache-manifest.18d55c17aced82bd1de70f080711e4f5.js
@@ -116,28 +116,28 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-Thin.2bf5104c.woff"
   },
   {
-    "revision": "90f4f06e361d9c08b9ae21939b7c0600",
-    "url": "/static/media/ProximaNovaCond-Regular.90f4f06e.woff"
-  },
-  {
     "revision": "0efcf5f69a608012bc8d4369604f3dd1",
     "url": "/static/media/ProximaNova-LightIt.0efcf5f6.woff"
   },
   {
-    "revision": "8576a0d0cd371d6767e5863c4fb1f75f",
-    "url": "/static/media/ProximaNovaCond-Black.8576a0d0.woff"
+    "revision": "90f4f06e361d9c08b9ae21939b7c0600",
+    "url": "/static/media/ProximaNovaCond-Regular.90f4f06e.woff"
   },
   {
     "revision": "ccf59c77109aecc8deeeede82e3bac87",
     "url": "/static/media/ProximaNova-Extrabld.ccf59c77.woff"
   },
   {
-    "revision": "ed0f04956fa1c43a330b91ac5e6d7068",
-    "url": "/static/media/ProximaNovaCond-ExtrabldIt.ed0f0495.woff"
+    "revision": "8576a0d0cd371d6767e5863c4fb1f75f",
+    "url": "/static/media/ProximaNovaCond-Black.8576a0d0.woff"
   },
   {
     "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
     "url": "/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "ed0f04956fa1c43a330b91ac5e6d7068",
+    "url": "/static/media/ProximaNovaCond-ExtrabldIt.ed0f0495.woff"
   },
   {
     "revision": "8feb512e78c18175c552af2be6ae2e02",
@@ -148,44 +148,44 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaCond-Semibold.f63773fd.woff"
   },
   {
-    "revision": "531859de6ac3d008c9125f8cf7faa34f",
-    "url": "/static/media/ProximaNovaExCn-Semibold.531859de.woff"
-  },
-  {
     "revision": "e22b9b8e9df91aebb85428144126d023",
     "url": "/static/media/ProximaNovaExCn-SemiboldIt.e22b9b8e.woff"
+  },
+  {
+    "revision": "531859de6ac3d008c9125f8cf7faa34f",
+    "url": "/static/media/ProximaNovaExCn-Semibold.531859de.woff"
   },
   {
     "revision": "0d23ced77b88ee2409417406f3d96ef0",
     "url": "/static/media/ProximaNova-ThinIt.0d23ced7.woff"
   },
   {
-    "revision": "ea36ec306ddeb6dcabf2bdf18de433f5",
-    "url": "/static/media/ProximaNovaExCn-ThinIt.ea36ec30.woff"
+    "revision": "755473efbd88e607ddcb20c60e320a04",
+    "url": "/static/media/ProximaNovaCond-Thin.755473ef.woff"
   },
   {
     "revision": "24200a65df49ad12b32f3ea2ec6e6ed7",
     "url": "/static/media/ProximaNovaCond-RegularIt.24200a65.woff"
   },
   {
-    "revision": "755473efbd88e607ddcb20c60e320a04",
-    "url": "/static/media/ProximaNovaCond-Thin.755473ef.woff"
-  },
-  {
-    "revision": "e04149190b633297448c17563a272f5b",
-    "url": "/static/media/ProximaNovaExCn-ExtrabldIt.e0414919.woff"
-  },
-  {
-    "revision": "c88bf6896c76bdf8d1b293e24763f424",
-    "url": "/static/media/ProximaNova-RegularIt.c88bf689.woff"
+    "revision": "ea36ec306ddeb6dcabf2bdf18de433f5",
+    "url": "/static/media/ProximaNovaExCn-ThinIt.ea36ec30.woff"
   },
   {
     "revision": "12fd4b2492b999a5a92b6b2f88da1f4d",
     "url": "/static/media/ProximaNova-SemiboldIt.12fd4b24.woff"
   },
   {
+    "revision": "c88bf6896c76bdf8d1b293e24763f424",
+    "url": "/static/media/ProximaNova-RegularIt.c88bf689.woff"
+  },
+  {
     "revision": "f0bf048f34f818c5fed5efa9eef4970d",
     "url": "/static/media/ProximaNovaCond-Extrabld.f0bf048f.woff"
+  },
+  {
+    "revision": "e04149190b633297448c17563a272f5b",
+    "url": "/static/media/ProximaNovaExCn-ExtrabldIt.e0414919.woff"
   },
   {
     "revision": "6e11383e61b983e0936738aa5d0b1c03",
@@ -200,40 +200,36 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-Light.e728f01e.woff"
   },
   {
-    "revision": "e93c2b7ef38a450060ef368d20ca1224",
-    "url": "/static/media/ProximaNovaExCn-LightIt.e93c2b7e.woff"
+    "revision": "d55029117e8168815c0dbe8d9a4b5177",
+    "url": "/static/media/ProximaNovaCond-BoldIt.d5502911.woff"
   },
   {
-    "revision": "b978d1b46a86df7a012cf8442936a1cb",
-    "url": "/static/media/ProximaNova-Black.b978d1b4.woff"
+    "revision": "e93c2b7ef38a450060ef368d20ca1224",
+    "url": "/static/media/ProximaNovaExCn-LightIt.e93c2b7e.woff"
   },
   {
     "revision": "ae8b34ed77b6f640b06bc5a149c8f0de",
     "url": "/static/media/ProximaNovaExCn-Extrabld.ae8b34ed.woff"
   },
   {
-    "revision": "d55029117e8168815c0dbe8d9a4b5177",
-    "url": "/static/media/ProximaNovaCond-BoldIt.d5502911.woff"
-  },
-  {
     "revision": "5b14909281652fdabbdd528524fd8021",
     "url": "/static/media/ProximaNovaExCn-BoldIt.5b149092.woff"
   },
   {
-    "revision": "2499eb7607015ec5967d0f3bf8750ae3",
-    "url": "/static/media/ProximaNovaT-Thin.2499eb76.woff"
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/static/media/ProximaNova-Bold.e2cf3dc2.woff"
   },
   {
     "revision": "966eb9cbbd1b789054ac77707d01e070",
     "url": "/static/media/ProximaNovaExCn-Bold.966eb9cb.woff"
   },
   {
-    "revision": "53fca16a9b49a5ac113eb81599d93a92",
-    "url": "/static/media/ProximaNovaExCn-Black.53fca16a.woff"
-  },
-  {
-    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
-    "url": "/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+    "revision": "2499eb7607015ec5967d0f3bf8750ae3",
+    "url": "/static/media/ProximaNovaT-Thin.2499eb76.woff"
   },
   {
     "revision": "af44a852757d1cac8f2ff15e6de18ae5",
@@ -244,8 +240,8 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaCond-Bold.d04bcdbe.woff"
   },
   {
-    "revision": "f7d3368adf113395b2b4c16d18344b13",
-    "url": "/static/media/ProximaNova-BlackIt.f7d3368a.woff"
+    "revision": "53fca16a9b49a5ac113eb81599d93a92",
+    "url": "/static/media/ProximaNovaExCn-Black.53fca16a.woff"
   },
   {
     "revision": "c4c6ffa8697c3e53f57735a2316c9092",
@@ -256,14 +252,6 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaCond-Regular.45e443b7.ttf"
   },
   {
-    "revision": "f4aee82e93e28c7bd99178e89f612a56",
-    "url": "/static/media/ProximaNovaCond-Black.f4aee82e.eot"
-  },
-  {
-    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
-    "url": "/static/media/ProximaNova-Extrabld.169bc0f1.eot"
-  },
-  {
     "revision": "51563919fd1af7376fac14b1194a8e4e",
     "url": "/static/media/ProximaNova-Extrabld.51563919.ttf"
   },
@@ -272,8 +260,8 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaCond-Black.e446a244.ttf"
   },
   {
-    "revision": "b7c512788e3c77b0196f0bace8a88418",
-    "url": "/static/media/ProximaNova-Regular.b7c51278.eot"
+    "revision": "f4aee82e93e28c7bd99178e89f612a56",
+    "url": "/static/media/ProximaNovaCond-Black.f4aee82e.eot"
   },
   {
     "revision": "cddac6589ca25eb44179341774222e79",
@@ -284,16 +272,12 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-Thin.31d025f3.eot"
   },
   {
-    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
-    "url": "/static/media/ProximaNova-Regular.7ce6760d.ttf"
-  },
-  {
     "revision": "df8c626474a73ab7a8b511655597c7c4",
     "url": "/static/media/ProximaNova-Semibold.df8c6264.ttf"
   },
   {
-    "revision": "9f855086c7d03fb2d45a3c5a24ce4a75",
-    "url": "/static/media/ProximaNovaCond-Semibold.9f855086.eot"
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/static/media/ProximaNova-Regular.7ce6760d.ttf"
   },
   {
     "revision": "508bce54da31972b6fbc95e283eb65c5",
@@ -302,6 +286,14 @@ self.__precacheManifest = [
   {
     "revision": "c8b3d95a11af22a5b19b83d4b6a00c91",
     "url": "/static/media/ProximaNovaCond-Semibold.c8b3d95a.ttf"
+  },
+  {
+    "revision": "9f855086c7d03fb2d45a3c5a24ce4a75",
+    "url": "/static/media/ProximaNovaCond-Semibold.9f855086.eot"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/static/media/ProximaNova-Regular.b7c51278.eot"
   },
   {
     "revision": "43451cf2507032dc01366c589e9bc487",
@@ -336,20 +328,12 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-Light.91f04efd.ttf"
   },
   {
-    "revision": "c9be7f03ba4e8215a9547ab9c52dafe2",
-    "url": "/static/media/ProximaNovaExCn-Extrabld.c9be7f03.eot"
-  },
-  {
     "revision": "17b18867a70f507a69bf31cd262d01e5",
     "url": "/static/media/ProximaNovaExCn-Extrabld.17b18867.ttf"
   },
   {
-    "revision": "4325ed848b66ed370420ea7fea09bc80",
-    "url": "/static/media/ProximaNova-Black.4325ed84.eot"
-  },
-  {
-    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
-    "url": "/static/media/ProximaNova-Bold.10141a9d.eot"
+    "revision": "c9be7f03ba4e8215a9547ab9c52dafe2",
+    "url": "/static/media/ProximaNovaExCn-Extrabld.c9be7f03.eot"
   },
   {
     "revision": "a0606c5fef28b9e57f501db027063141",
@@ -368,28 +352,28 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-Bold.00063272.ttf"
   },
   {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
     "revision": "9b75d58eb57d6104c1b4f1aafc7790f7",
     "url": "/static/media/ProximaNovaCond-Bold.9b75d58e.eot"
   },
   {
-    "revision": "4a705b08982e53acfd088a0095e2bbed",
-    "url": "/static/media/ProximaNovaCond-Bold.4a705b08.ttf"
-  },
-  {
-    "revision": "50e304904ef79cabd5cdce7577b776ee",
-    "url": "/static/media/ProximaNovaExCn-Black.50e30490.eot"
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/static/media/ProximaNova-Bold.10141a9d.eot"
   },
   {
     "revision": "4082bf05622e009482a05c2a4a6045ed",
     "url": "/static/media/ProximaNova-LightIt.4082bf05.ttf"
   },
   {
-    "revision": "4e4c807a7584e8f9f777e0a203809643",
-    "url": "/static/media/ProximaNovaCond-ExtrabldIt.4e4c807a.eot"
-  },
-  {
     "revision": "831f0fd5af3ffaec400d9800682710fa",
     "url": "/static/media/ProximaNovaCond-ExtrabldIt.831f0fd5.ttf"
+  },
+  {
+    "revision": "4e4c807a7584e8f9f777e0a203809643",
+    "url": "/static/media/ProximaNovaCond-ExtrabldIt.4e4c807a.eot"
   },
   {
     "revision": "e538ddff090e9eedd7662b6793cd531e",
@@ -404,16 +388,12 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-ThinIt.c951eefe.eot"
   },
   {
-    "revision": "a2372b29f9f05344b1546c8f6f91a320",
-    "url": "/static/media/ProximaNovaExCn-ThinIt.a2372b29.ttf"
+    "revision": "f5e20c8d5ef5229e7bebd8d27ea11b9a",
+    "url": "/static/media/ProximaNova-SemiboldIt.f5e20c8d.eot"
   },
   {
     "revision": "5f5d2168729682606d59e164739146f0",
     "url": "/static/media/ProximaNova-RegularIt.5f5d2168.eot"
-  },
-  {
-    "revision": "f5e20c8d5ef5229e7bebd8d27ea11b9a",
-    "url": "/static/media/ProximaNova-SemiboldIt.f5e20c8d.eot"
   },
   {
     "revision": "76050cd071680d656e0ca48c6dc6085f",
@@ -428,16 +408,16 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-ExtrabldIt.69ddbf29.eot"
   },
   {
-    "revision": "053bb2a38c55d3072a220573441a889c",
-    "url": "/static/media/ProximaNovaExCn-ExtrabldIt.053bb2a3.ttf"
-  },
-  {
     "revision": "7ec9ee04129bc2256fb1da68d986a35b",
     "url": "/static/media/ProximaNovaExCn-BlackIt.7ec9ee04.eot"
   },
   {
-    "revision": "a0fb7c8c2b4d5864d5e7fb43e4ee8c56",
-    "url": "/static/media/ProximaNovaExCn-BlackIt.a0fb7c8c.ttf"
+    "revision": "a2372b29f9f05344b1546c8f6f91a320",
+    "url": "/static/media/ProximaNovaExCn-ThinIt.a2372b29.ttf"
+  },
+  {
+    "revision": "053bb2a38c55d3072a220573441a889c",
+    "url": "/static/media/ProximaNovaExCn-ExtrabldIt.053bb2a3.ttf"
   },
   {
     "revision": "6dbeb4c3bbe66954b5a2485d64915ff3",
@@ -452,6 +432,10 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaCond-BoldIt.d78ecc81.eot"
   },
   {
+    "revision": "a0fb7c8c2b4d5864d5e7fb43e4ee8c56",
+    "url": "/static/media/ProximaNovaExCn-BlackIt.a0fb7c8c.ttf"
+  },
+  {
     "revision": "952bd32c42d8c256bbe90d167d1d0049",
     "url": "/static/media/ProximaNovaCond-BoldIt.952bd32c.ttf"
   },
@@ -464,22 +448,6 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-BoldIt.3b25063d.ttf"
   },
   {
-    "revision": "54fe7d558c6478388a89744c25fd5514",
-    "url": "/static/media/ProximaNova-BoldIt.54fe7d55.eot"
-  },
-  {
-    "revision": "c8516677b967f1e3212e6274da5be696",
-    "url": "/static/media/ProximaNovaT-Thin.c8516677.eot"
-  },
-  {
-    "revision": "4a3961960c6827bf176eb0ef73653784",
-    "url": "/static/media/ProximaNovaT-Thin.4a396196.ttf"
-  },
-  {
-    "revision": "0e18c5b3dd6cfbb1d32c15b4c9cc8106",
-    "url": "/static/media/ProximaNova-BoldIt.0e18c5b3.ttf"
-  },
-  {
     "revision": "4f987e179211965a646ec40f916058cb",
     "url": "/static/media/ProximaNova-LightIt.4f987e17.eot"
   },
@@ -488,16 +456,16 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNova-ThinIt.2b7c2fd0.eot"
   },
   {
-    "revision": "2b8aab378fb0ecdd6d58e97ceea0100b",
-    "url": "/static/media/ProximaNova-ThinIt.2b8aab37.ttf"
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/static/media/ProximaNova-Extrabld.169bc0f1.eot"
   },
   {
     "revision": "a4938ebf5784ea49e4e5ea53dc69a0cb",
     "url": "/static/media/ProximaNovaCond-RegularIt.a4938ebf.eot"
   },
   {
-    "revision": "56ba7fbde049d61e191f46a152dde2bd",
-    "url": "/static/media/ProximaNovaCond-RegularIt.56ba7fbd.ttf"
+    "revision": "2b8aab378fb0ecdd6d58e97ceea0100b",
+    "url": "/static/media/ProximaNova-ThinIt.2b8aab37.ttf"
   },
   {
     "revision": "c337e8fcdc6eb4aa46d5fc6625ec4d63",
@@ -506,6 +474,34 @@ self.__precacheManifest = [
   {
     "revision": "ce7389680ca26cf2056f3111690e2624",
     "url": "/static/media/ProximaNovaCond-ThinIt.ce738968.ttf"
+  },
+  {
+    "revision": "56ba7fbde049d61e191f46a152dde2bd",
+    "url": "/static/media/ProximaNovaCond-RegularIt.56ba7fbd.ttf"
+  },
+  {
+    "revision": "c8516677b967f1e3212e6274da5be696",
+    "url": "/static/media/ProximaNovaT-Thin.c8516677.eot"
+  },
+  {
+    "revision": "54fe7d558c6478388a89744c25fd5514",
+    "url": "/static/media/ProximaNova-BoldIt.54fe7d55.eot"
+  },
+  {
+    "revision": "0e18c5b3dd6cfbb1d32c15b4c9cc8106",
+    "url": "/static/media/ProximaNova-BoldIt.0e18c5b3.ttf"
+  },
+  {
+    "revision": "4a705b08982e53acfd088a0095e2bbed",
+    "url": "/static/media/ProximaNovaCond-Bold.4a705b08.ttf"
+  },
+  {
+    "revision": "4a3961960c6827bf176eb0ef73653784",
+    "url": "/static/media/ProximaNovaT-Thin.4a396196.ttf"
+  },
+  {
+    "revision": "f7d3368adf113395b2b4c16d18344b13",
+    "url": "/static/media/ProximaNova-BlackIt.f7d3368a.woff"
   },
   {
     "revision": "8a742ee77d45f7df8d05fc4410d4fb19",
@@ -536,6 +532,10 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-RegularIt.e1ec0410.woff"
   },
   {
+    "revision": "50e304904ef79cabd5cdce7577b776ee",
+    "url": "/static/media/ProximaNovaExCn-Black.50e30490.eot"
+  },
+  {
     "revision": "77aed8ec40b789d1ceafb0fa7dabccf8",
     "url": "/static/media/ProximaNova-BlackIt.77aed8ec.ttf"
   },
@@ -544,44 +544,40 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNovaExCn-Regular.44d67cdb.ttf"
   },
   {
-    "revision": "22d35e169590a9e2f45f4216aeb7d66f",
-    "url": "/static/media/ProximaNovaExCn-Regular.22d35e16.eot"
-  },
-  {
-    "revision": "a9ee33c2fcc35e78aafee1e86170f9be",
-    "url": "/static/media/ProximaNova-BlackIt.a9ee33c2.eot"
-  },
-  {
     "revision": "e72c6a1c67617337a21ada73fd8d3a3e",
     "url": "/static/media/ProximaNovaCond-Light.e72c6a1c.ttf"
-  },
-  {
-    "revision": "f5dd0d63e5320e76dd3952ad4cff5a95",
-    "url": "/static/media/ProximaNovaExCn-Black.f5dd0d63.ttf"
-  },
-  {
-    "revision": "80bd50e92f14ae588fc37fc941e35a2f",
-    "url": "/static/media/ProximaNovaCond-SemiboldIt.80bd50e9.eot"
   },
   {
     "revision": "5177fabdfcff0a8d571d9c2d588ea8fb",
     "url": "/static/media/ProximaNovaCond-SemiboldIt.5177fabd.ttf"
   },
   {
-    "revision": "0188899cd3ec92cdcaa4c97ca0c75247",
-    "url": "/static/media/ProximaNova-Light.0188899c.ttf"
+    "revision": "80bd50e92f14ae588fc37fc941e35a2f",
+    "url": "/static/media/ProximaNovaCond-SemiboldIt.80bd50e9.eot"
+  },
+  {
+    "revision": "a9ee33c2fcc35e78aafee1e86170f9be",
+    "url": "/static/media/ProximaNova-BlackIt.a9ee33c2.eot"
+  },
+  {
+    "revision": "200afa6387fc6e666c54cbf04813d0e4",
+    "url": "/static/media/ProximaNova-ExtrabldIt.200afa63.ttf"
   },
   {
     "revision": "d778218590f14376c366c515d384808e",
     "url": "/static/media/ProximaNova-Light.d7782185.eot"
   },
   {
-    "revision": "a05b818064f899c86c464675bc48e083",
-    "url": "/static/media/ProximaNovaCond-BlackIt.a05b8180.eot"
+    "revision": "0188899cd3ec92cdcaa4c97ca0c75247",
+    "url": "/static/media/ProximaNova-Light.0188899c.ttf"
   },
   {
-    "revision": "200afa6387fc6e666c54cbf04813d0e4",
-    "url": "/static/media/ProximaNova-ExtrabldIt.200afa63.ttf"
+    "revision": "22d35e169590a9e2f45f4216aeb7d66f",
+    "url": "/static/media/ProximaNovaExCn-Regular.22d35e16.eot"
+  },
+  {
+    "revision": "a05b818064f899c86c464675bc48e083",
+    "url": "/static/media/ProximaNovaCond-BlackIt.a05b8180.eot"
   },
   {
     "revision": "8ffbb5f5f78572f71c4473ba968595e6",
@@ -592,12 +588,16 @@ self.__precacheManifest = [
     "url": "/static/media/ProximaNova-ExtrabldIt.6ba3d757.eot"
   },
   {
-    "revision": "90c38adeb3d485762da65685f1978a82",
-    "url": "/static/media/ProximaNovaExCn-RegularIt.90c38ade.ttf"
-  },
-  {
     "revision": "48aa3a15fc52dda835a05988c23ca260",
     "url": "/static/media/ProximaNovaCond-Light.48aa3a15.eot"
+  },
+  {
+    "revision": "f5dd0d63e5320e76dd3952ad4cff5a95",
+    "url": "/static/media/ProximaNovaExCn-Black.f5dd0d63.ttf"
+  },
+  {
+    "revision": "90c38adeb3d485762da65685f1978a82",
+    "url": "/static/media/ProximaNovaExCn-RegularIt.90c38ade.ttf"
   },
   {
     "revision": "eb56098339e50096f0558f01f9036cf6",
@@ -616,12 +616,12 @@ self.__precacheManifest = [
     "url": "/static/media/slick.b7c9e1e4.woff"
   },
   {
-    "revision": "d41f55a78e6f49a5512878df1737e58a",
-    "url": "/static/media/slick.d41f55a7.ttf"
-  },
-  {
     "revision": "f97e3bbf73254b0112091d0192f17aec",
     "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
   },
   {
     "revision": "b569e232994a559fcc0e7dad5b892035",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f673c2b1aeca7ae054c6143468fdf933.js"
+  "/precache-manifest.18d55c17aced82bd1de70f080711e4f5.js"
 );
 
 workbox.clientsClaim();
